6f23aa8ebedb22bdfb1a114fa7412349176b36ea
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-11 21:12:34 +0600

    get all countries data from api

--- react-tour/src/components/cuntries.jsx
+++ react-tour/src/components/cuntries.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 const Countries = () => {
     const [Countries, setCountries] = useState([]);
+    useEffect(()=>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    },([]));
     return (
         <div>
             <h3>Countries</h3>
