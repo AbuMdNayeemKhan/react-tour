368fbf303fdaa92f42d434dfd0d5421b0ce1573b
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-11 20:13:33 +0600

    design done

---
+++ react-tour/src/components/cuntries.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Countries = () => {
+    const [Countries, setCountries] = useState([]);
+    return (
+        <div>
+            <h3>Countries</h3>
+        </div>
+    )
+};
+
+export default Countries;
