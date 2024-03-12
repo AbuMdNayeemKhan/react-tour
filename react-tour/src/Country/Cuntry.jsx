74f879f8afb1e3fa8dbcc04328a10de5afd286b2
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-12 22:21:40 +0600

    push data asa props

--- react-tour/src/Country/Cuntry.jsx
+++ react-tour/src/Country/Cuntry.jsx
@@ -2,19 +2,19 @@ import { useState } from 'react';
 import './Contry.css';
 
 
-const Cuntry = ({country}) => {
-    const {name, flags, handlevisitedCountires} = country;
+const Cuntry = ({country, handlevisitedCountires}) => {
+    const {name, flags} = country;
     const [visited, setVisited] = useState(false);
     const visitedHandeler = () =>{
         setVisited(!visited);
     }
-    
+    console.log(handlevisitedCountires);
     return (
         <div className={`country ${visited ? 'visited' : ''}`}>
             <img src={flags.png} alt="" className='flags' />
             <h3>Name:{name.common}</h3>
             <button onClick={visitedHandeler}>{visited && 'visited' || 'visite'}</button>
-            <button>set as visited</button>
+            <button onClick={handlevisitedCountires}>set as visited</button>
         </div>
     );
 };
