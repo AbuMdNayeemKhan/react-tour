f05ba43db9f4c574f2fc0b1d3bbc9cff01f872c2
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-12 21:56:55 +0600

    added data form cuntries to country

--- react-tour/src/Country/Cuntry.jsx
+++ react-tour/src/Country/Cuntry.jsx
@@ -3,16 +3,18 @@ import './Contry.css';
 
 
 const Cuntry = ({country}) => {
-    const {name, flags} = country;
+    const {name, flags, handlevisitedCountires} = country;
     const [visited, setVisited] = useState(false);
     const visitedHandeler = () =>{
         setVisited(!visited);
     }
+    
     return (
         <div className={`country ${visited ? 'visited' : ''}`}>
             <img src={flags.png} alt="" className='flags' />
             <h3>Name:{name.common}</h3>
             <button onClick={visitedHandeler}>{visited && 'visited' || 'visite'}</button>
+            <button>set as visited</button>
         </div>
     );
 };
--- react-tour/src/components/cuntries.jsx
+++ react-tour/src/components/cuntries.jsx
@@ -5,18 +5,30 @@ import './Cuntries.css'
 
 const Countries = () => {
     const [country, setCountry] = useState([]);
+    const [visitedCountires, setvisitedCountires] = useState([]);
     useEffect(()=>{
         fetch("https://restcountries.com/v3.1/all")
         .then(res => res.json())
         .then(data => setCountry(data))
     },[])
 
+    const handlevisitedCountires = (country) =>{
+        console.log('add this to your visited country');
+        console.log(country);
+    }
+
     return(
         <div>
             <h3 className="text-center">Countries: {country.length}</h3>
+            <div>
+                <h5>Visited Country</h5>
+                <ul>
+
+                </ul>
+            </div>
             <div className="country-container">
                 {
-                    country.map(country => <Cuntry key={country.cca3} country={country}></Cuntry>)
+                    country.map(country => <Cuntry key={country.cca3} handlevisitedCountires={handlevisitedCountires} country={country}></Cuntry>)
                 }
             </div>
         </div>
