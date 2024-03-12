11783b17f9f2aa3c72c118ed50411cbc25153eb0
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-12 20:36:57 +0600

    react useState

--- react-tour/src/Country/Cuntry.jsx
+++ react-tour/src/Country/Cuntry.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './Contry.css';
 
 
 const Cuntry = ({country}) => {
     const {name, flags} = country;
+    const [visited, setVisited] = useState(false);
+    const visitedHandeler = () =>{
+        setVisited(!visited);
+    }
     return (
         <div className='country'>
             <img src={flags.png} alt="" className='flags' />
             <h3>Name:{name.common}</h3>
+            <button onClick={visitedHandeler}>{visited && 'visited' || 'visite'}</button>
         </div>
     );
 };
--- react-tour/src/components/cuntries.jsx
+++ react-tour/src/components/cuntries.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Cuntry from "../Country/Cuntry";
+import './Cuntries.css'
 
 const Countries = () => {
     const [country, setCountry] = useState([]);
@@ -12,9 +13,12 @@ const Countries = () => {
 
     return(
         <div>
-            {
-                country.map(country => <Cuntry key={country.cca3} country={country}></Cuntry>)
-            }
+            <h3 className="text-center">Countries: {country.length}</h3>
+            <div className="country-container">
+                {
+                    country.map(country => <Cuntry key={country.cca3} country={country}></Cuntry>)
+                }
+            </div>
         </div>
     )
 };
